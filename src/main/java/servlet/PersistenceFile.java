bcdb818751d06a2935c7d07582edc7e48cda38cb
Bhupinder Singh <48160814+B-Singh2020@users.noreply.github.com>
2020-10-13 18:43:02 -0400

    fixed file

--- src/main/java/servlet/PersistenceFile.java
+++ src/main/java/servlet/PersistenceFile.java
@@ -84,7 +84,7 @@ public class PersistenceFile extends HttpServlet{
 
      if (error.length() == 0){
        PrintWriter entriesPrintWriter = new PrintWriter(new FileWriter(RESOURCE_FILE, true), true);
-       entriesPrintWriter.println(name+VALUE_SEPARATOR+age);
+       entriesPrintWriter.println(name+VALUE_SEPARATOR+age+VALUE_SEPARATOR+major);
        entriesPrintWriter.close();
 
        PrintHead(out);
