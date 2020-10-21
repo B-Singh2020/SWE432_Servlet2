7f397efd0207dc8c3a77dc7b08b8b5d6cdb20a92
Bhupinder Singh <48160814+B-Singh2020@users.noreply.github.com>
2020-10-21 14:13:42 -0400

    hehe

--- src/main/java/servlet/AttributeServlet.java
+++ src/main/java/servlet/AttributeServlet.java
@@ -36,6 +36,7 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
       if ((name != null && name.length() > 0) && (value != null && value.length() > 0))
       {
          session.setAttribute(name, value);
+         session.setAttribute(food, drink);
       }
 
    }
@@ -82,11 +83,18 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
    {
       String att_name  = (String) e.nextElement();
       String att_value = (String) session.getAttribute(att_name);
-     
+      String att_food = (String) session.getAttribute(att_food);
+      String att_drink = (String) session.getAttribute(att_drink);
+      
       out.print  ("<br><b>Name:</b> ");
       out.println(att_name);
       out.print  ("<br><b>Value:</b> ");
       out.println(att_value);
+      
+      out.print  ("<br><b>Food:</b> ");
+      out.println(att_food);
+      out.print  ("<br><b>Drink:</b> ");
+      out.println(att_drink);
       
      
    } //end while
