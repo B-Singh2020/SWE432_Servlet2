75f1377bdcc632f8f5cd0205c75f04d13ab2a6e1
Bhupinder Singh <48160814+B-Singh2020@users.noreply.github.com>
2020-11-03 17:28:49 -0500

    t

--- src/main/java/servlet/LogicalPredicateCalculator.java
+++ src/main/java/servlet/LogicalPredicateCalculator.java
@@ -301,6 +301,7 @@ public class LogicalPredicateCalculator extends HttpServlet // Inheriting from H
 				"    </table>\n" + 
 				"    <p class=\"error-text\" id=\"varErrorMessage\"> </p>\n" + 
 				"    <input type=\"submit\" id=\"submit\" onclick=\"getOps()\" disabled name=\"Inputted\">\n" + 
+				"    <input type=\"submit\" id=\"submit\" onclick=\"getOps()\" disabled name=\"Inputted\">\n" +
 				"  </form>\n" + 
 				"\n" + 
 				"\n" + 
@@ -314,11 +315,11 @@ public class LogicalPredicateCalculator extends HttpServlet // Inheriting from H
 	public void doPost (HttpServletRequest request, HttpServletResponse response)
 	   throws ServletException, IOException
 	{
-		String val1 = "";
+		String val1 = request.getParameter("ops1");;
 		String val2 = request.getParameter("Inputted");
 		
 		response.setContentType("text/html");
 		   PrintWriter out = response.getWriter();
-		   out.println(val2);
+		   out.println(val1 + val2);
 	}
 }  
